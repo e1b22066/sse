2f69aa6dba390939a04a0c9f8201e9f463707521
e1b22066 <e1b22066@oit.ac.jp>
2024-10-22 16:40:49 +0900

    購入処理

--- src/main/java/oit/is/z2722/sse/controller/Sample57Controller.java
+++ src/main/java/oit/is/z2722/sse/controller/Sample57Controller.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import oit.is.z2722.sse.model.Fruit;
@@ -29,6 +29,20 @@ public class Sample57Controller {
   public String sample57(ModelMap model) {
     final ArrayList<Fruit> fruits7 = shop57.syncShowFruitsList();
     model.addAttribute("fruits7", fruits7);
+    return "sample57.html";
+  }
+
+  @GetMapping("step8")
+  @Transactional
+  public String sample58(@RequestParam Integer id, ModelMap model) {
+    // 選択したフルーツを削除し，削除対象のフルーツをmodelに登録
+    final Fruit fruit8 = this.shop57.syncBuyFruits(id);
+    model.addAttribute("fruit8", fruit8);
+
+    // 残りのフルーツリストを取得してmodelに登録
+    final ArrayList<Fruit> fruits7 = shop57.syncShowFruitsList();
+    model.addAttribute("fruits7", fruits7);
+
     return "sample57.html";
   }
 }
